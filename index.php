8add21b14b68198429eb3399de155fc0d2de5abd
MeiKatsuragi <yoshiharuishihara2@gmail.com>
2025-05-09 14:19:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $Cpass_err = "";
 $error = false;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];  // Fixed: assign to $username
+    $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $confirmed_pass = $_POST['Cpass'];
@@ -74,47 +74,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="index.css">
-
-    <title>Sign Up</title>
+    <title>Register - BookRec</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="title"> Sign Up</div>
-        <form action="" method="POST">
-            <div class="user-details">
-                <div class="input-box">
-                    <span class="details">Username</span>
-                    <input type="text" placeholder=" Enter Username" name="username" required>
-                </div>
-                <div class="input-box">
-            <span class="details">Email</span>
-            <input type="email" name="email" required>
+    <div class="form-container">
+        <h2>Register for BookRec 📚</h2>
+        <form method="POST" action="">
+            <input type="text" name="username" placeholder="Username" value="<?= htmlspecialchars($username) ?>">
+            <p class="error"><?= $username_err ?></p>
 
-            <span class="details">Password</span>
-            <input type="password" name="password" minlength="6" required>
-            
-            <span class="details">Confirm Password</span>
-            <input type="password" id="Cpass" name="Cpass" class="textbox" placeholder="Re-enter password here" required>
-            <span style="color:red;"><?php echo $Cpass_err; ?></span><br><br>
-            </div>
-                <div class="button">
-                    <input type="submit" value="Register">
-                </div>
+            <input type="email" name="email" placeholder="Email" value="<?= htmlspecialchars($email) ?>">
+            <p class="error"><?= $email_err ?></p>
 
-                <div class="last">
-                    <a href="login.php">Already have an account?</a>
-                </div>
-            
+            <input type="password" name="password" placeholder="Password">
+            <p class="error"><?= $pass_err ?></p>
+
+            <input type="password" name="Cpass" placeholder="Confirm Password">
+            <p class="error"><?= $Cpass_err ?></p>
+
+            <button type="submit">Register</button>
+            <p class="switch">Already have an account? <a href="login.php">Login here</a>.</p>
         </form>
     </div>
-    
 </body>
 </html>
